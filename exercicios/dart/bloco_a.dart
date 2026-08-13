ee5e4412bf6eacefaec004411d02c8bf38d08e7a
Tiago Cardoso Ferreira <tiagocardoso1357@gmail.com>
2026-08-12 22:25:51 -0300

    a1

---
+++ exercicios/dart/bloco_a.dart
@@ -0,0 +1,12 @@
+// A1
+
+void main() {
+  final String producerName = "João Silva";
+  final String city = "Sorriso";
+  final double areaInHectares = 150.5;
+  final int harvestYear = 2024;
+
+  print(
+    'Produtor: $producerName | Cidade: $city | Área: $areaInHectares ha | Safra: $harvestYear'
+  );
+}
